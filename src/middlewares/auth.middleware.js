e56688b972c97a16a08157acdde1f1eff2a80127
AbhishekJadli007 <abhishekjadli2004@gmail.com>
2025-09-06 14:55:29 +0530

    fixed import statements in auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
-import { ApiError } from "../utils/apierror";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import User from "../models/user.model.js";
+import { ApiError } from "../utils/apierror.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import {User} from "../models/user.model.js";
 import jwt from "jsonwebtoken"
 
  const VerifyJWT = asyncHandler(async (req,res,next) =>{
